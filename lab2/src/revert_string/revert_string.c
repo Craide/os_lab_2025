2f584d940cce7368f4d2b219dfd048d0610c97b0
CRAIDE <33939620+Craide@users.noreply.github.com>
2025-10-04 12:39:12 +0000

    lr2

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -1,7 +1,21 @@
 #include "revert_string.h"
+#include <string.h>
 
-void RevertString(char *str)
+void RevertString(char *sequence)
 {
-	// your code here
+    if (!sequence) return;
+    
+    size_t sequence_length = strlen(sequence);
+    size_t left_index = 0;
+    size_t right_index = sequence_length - 1;
+    
+    while (left_index < right_index)
+    {
+        char temporary_holder = sequence[left_index];
+        sequence[left_index] = sequence[right_index];
+        sequence[right_index] = temporary_holder;
+        
+        left_index++;
+        right_index--;
+    }
 }
-
